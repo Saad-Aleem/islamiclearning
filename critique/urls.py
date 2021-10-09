e5fba4074d946df639b6821511529ee813665fc2
Adam-96 <70057586+Adam-96@users.noreply.github.com>
2021-10-09 09:36:24 +0500

    Update urls.py

--- critique/urls.py
+++ critique/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='critique'),
